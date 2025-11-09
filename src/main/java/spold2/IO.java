2728ff42b3c8b67f5e2e2480280b577e98e2b1a3
msrocka <michael.srocka@gmail.com>
2025-11-09 14:59:18 +0100

    make IO package private

--- src/main/java/spold2/IO.java
+++ src/main/java/spold2/IO.java
@@ -1,16 +1,16 @@
 package spold2;
 
-import jakarta.xml.bind.JAXB;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class IO {
+import jakarta.xml.bind.JAXB;
 
-	public static <T> T read(File file, Class<T> type) {
+class IO {
+
+	static <T> T read(File file, Class<T> type) {
 		try (FileInputStream stream = new FileInputStream(file)) {
 			return read(stream, type);
 		} catch (Exception e) {
@@ -19,7 +19,7 @@ public class IO {
 		}
 	}
 
-	public static <T> T read(InputStream is, Class<T> type) {
+	static <T> T read(InputStream is, Class<T> type) {
 		try {
 			return JAXB.unmarshal(is, type);
 		} catch (Exception e) {
@@ -28,7 +28,7 @@ public class IO {
 		}
 	}
 
-	public static void write(Object obj, File file) {
+	static void write(Object obj, File file) {
 		try (FileOutputStream fos = new FileOutputStream(file)) {
 			write(obj, fos);
 		} catch (Exception e) {
@@ -37,7 +37,7 @@ public class IO {
 		}
 	}
 
-	public static void write(Object obj, OutputStream out) {
+	static void write(Object obj, OutputStream out) {
 		try {
 			JAXB.marshal(obj, out);
 		} catch (Exception e) {
